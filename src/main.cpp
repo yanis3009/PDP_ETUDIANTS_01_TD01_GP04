2526f5f7d687e88a04b8c52c6de3bb87e7c3f0de
Yanis GUERBAA <yanis.guerbaa@edu.ece.fr>
2025-01-22 11:01:27 +0100

    blabla

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
+// Define the pins that we will use
+#define CAPTEUR 33
+#define LED 26
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // put your code here to run repeatedly
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+  delay(1000);
 }
